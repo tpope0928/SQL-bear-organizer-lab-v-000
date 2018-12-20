1e82254670524c8c9c8b9d50315788b5699c01cc
Thomas Pope <tpope0928@gmail.com>
2018-12-20 21:39:05 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -5,5 +5,5 @@ CREATE TABLE bears (
                 gender CHAR(1),
                 color TEXT,
                 temperment TEXT,
-                alive 
+                alive BOOLEAN,
             );
