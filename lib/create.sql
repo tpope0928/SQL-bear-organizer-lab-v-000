4ffc2b482584aafd2f1672d42e9f64c82bfd1eb9
Thomas Pope <tpope0928@gmail.com>
2018-12-20 21:40:17 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -1,10 +1,9 @@
 CREATE TABLE bears (
-        id INTEGER PRIMARY KEY,
-                name TEXT, 
-                age INTEGER,
-                gender CHAR(1),
-                color TEXT,
-                temperment TEXT,
-                alive BOOLEAN
-                
-            );
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  age INTEGER,
+  gender CHAR(1),
+  color TEXT,
+  temperament TEXT,
+  alive BOOLEAN
+);
